78f20ba897d7aaff57c29bc1d045e6b8ff74ebc9
Mauro Colella <mauro.ludovico.colella@gmail.com>
2022-12-21 11:15:10 +0800

    Ensure custom fonts are injected successfully

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,15 @@ import 'node_modules/normalize.css/normalize.css'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { PT_Sans } from '@next/font/google'
-const ptSans = PT_Sans({ subsets: ['latin'], weight: '400' })
+const ptSans = PT_Sans({ subsets: ['latin'], weight: ['400', '700'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <style jsx global>{`
+        html {
+          font-family: ${ptSans.style.fontFamily}, sans-serif;
+        }
+      `}</style>
+    <Component {...pageProps} />
+  </>
 }
