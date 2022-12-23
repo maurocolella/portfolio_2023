6803b141db5d468f5ce7d4036884a7022ffcccd5
Mauro Colella <mauro.ludovico.colella@gmail.com>
2022-12-23 21:07:12 +0800

    Configure basic layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,9 @@
-import 'node_modules/normalize.css/normalize.css'
-import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import { PT_Sans } from '@next/font/google'
+
+import 'node_modules/normalize.css/normalize.css'
+import '../styles/globals.scss'
+import { DefaultLayout } from '@/containers/DefaultLayout'
 const ptSans = PT_Sans({ subsets: ['latin'], weight: ['400', '700'] })
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -11,6 +13,8 @@ export default function App({ Component, pageProps }: AppProps) {
           font-family: ${ptSans.style.fontFamily}, sans-serif;
         }
       `}</style>
-    <Component {...pageProps} />
+    <DefaultLayout>
+      <Component {...pageProps} />
+    </DefaultLayout>
   </>
 }
