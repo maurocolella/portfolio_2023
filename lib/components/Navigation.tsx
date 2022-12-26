ab1073da41e3d34a49cd3d87a3b91e8dad43f9a1
Mauro Colella <mauro.ludovico.colella@gmail.com>
2022-12-26 21:09:42 +0800

    Implement basic navigation highlight

--- lib/components/Navigation.tsx
+++ lib/components/Navigation.tsx
@@ -1,24 +1,51 @@
+'use client'
+
+import { useState } from 'react'
 import { motion } from 'framer-motion'
 import Link from 'next/link'
-import styles from '../../styles/PageLayout.module.scss'
+import styles from '../../styles/Navigation.module.scss'
 
 export const Navigation: React.FC = () => {
+  const [selected, setSelected] = useState(0)
+
   return <nav className={styles.nav}>
+
     <motion.ul
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ ease: 'linear', duration: 0.5, delay: 1 }}
       className={styles.nav__menu}
     >
-      <li className={styles.nav__cell}>
-        <Link className={styles.nav__link} href="/">Home</Link>
-      </li>
-      <li className={styles.nav__cell}>
-        <Link className={styles.nav__link} href="/about">About</Link>
-      </li>
-      <li className={styles.nav__cell}>
-        <a className={styles.nav__link} href="#">Blog</a>
-      </li>
+      <motion.li className={`${styles.nav__cell} ${selected === 0 && styles['nav__cell-selected']}`} key={0}>
+        <Link
+          className={styles.nav__link}
+          href="/"
+          onClick={() => setSelected(0)}
+        >
+          Home
+        </Link>
+        {selected === 0 && <motion.div className={styles.nav__underline} />}
+      </motion.li>
+      <motion.li className={`${styles.nav__cell} ${selected === 1 && styles['nav__cell-selected']}`} key={1}>
+        <Link
+          className={styles.nav__link}
+          href="/about"
+          onClick={() => setSelected(1)}
+        >
+          About
+        </Link>
+        {selected === 1 && <motion.div className={styles.nav__underline} />}
+      </motion.li>
+      <motion.li className={`${styles.nav__cell} ${selected === 2 && styles['nav__cell-selected']}`} key={2}>
+        <a
+          className={styles.nav__link}
+          href="#"
+          onClick={() => setSelected(2)}
+        >
+          Blog
+        </a>
+        {selected === 2 && <motion.div className={styles.nav__underline} />}
+      </motion.li>
     </motion.ul>
   </nav>
 }
