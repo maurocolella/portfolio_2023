d68f33e94ace88e2b4a6905fc026b520a5179cae
Mauro Colella <mauro.ludovico.colella@gmail.com>
2023-01-03 13:40:17 +0800

    Remove opacity from effects + switch runtime

--- lib/components/Navigation.tsx
+++ lib/components/Navigation.tsx
@@ -1,48 +1,54 @@
 import { useState } from 'react'
-import { motion } from 'framer-motion'
+import { motion, LayoutGroup } from 'framer-motion'
 import Link from 'next/link'
 import styles from '../../styles/Navigation.module.scss'
 
 export const Navigation: React.FC = () => {
   const [selected, setSelected] = useState(0)
 
+  const entries = [
+    {
+      label: 'Home',
+      href: '/'
+    },
+    {
+      label: 'About',
+      href: '/about'
+    },
+    {
+      label: 'Blog',
+      href: '#'
+    },
+  ]
+
   return <nav className={styles.nav}>
-    <motion.ul
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      transition={{ ease: 'linear', duration: 0.5, delay: 1 }}
+    <ul
       className={styles.nav__menu}
     >
-      <motion.li className={`${styles.nav__cell} ${selected === 0 && styles['nav__cell-selected']}`} key={0}>
-        <Link
-          className={styles.nav__link}
-          href="/"
-          onClick={() => setSelected(0)}
-        >
-          Home
-        </Link>
-        {selected === 0 && <motion.div layoutId="underline" className={styles.nav__underline} />}
-      </motion.li>
-      <motion.li className={`${styles.nav__cell} ${selected === 1 && styles['nav__cell-selected']}`} key={1}>
-        <Link
-          className={styles.nav__link}
-          href="/about"
-          onClick={() => setSelected(1)}
-        >
-          About
-        </Link>
-        {selected === 1 && <motion.div layoutId="underline" className={styles.nav__underline} />}
-      </motion.li>
-      <motion.li className={`${styles.nav__cell} ${selected === 2 && styles['nav__cell-selected']}`} key={2}>
-        <a
-          className={styles.nav__link}
-          href="#"
-          onClick={() => setSelected(2)}
-        >
-          Blog
-        </a>
-        {selected === 2 && <motion.div layoutId="underline" className={styles.nav__underline} />}
-      </motion.li>
-    </motion.ul>
+      {entries.map((entry, id) =>
+        <motion.li className={`${styles.nav__cell} ${selected === id && styles['nav__cell-selected']}`} key={id}>
+          <Link
+            href={entry.href}
+            legacyBehavior
+          >
+            <motion.a
+              className={styles.nav__link}
+              initial={{ color: '#fff' }}
+              animate={{ color: selected === id ? '#09c' : '#111' }}
+              transition={{ ease: 'linear', duration: 0.5, delay: 1 }}
+              onClick={() => setSelected(id)}
+            >
+            {entry.label}
+            </motion.a>
+          </Link>
+          {selected === id && <motion.div
+            layoutId="underline"
+            className={styles.nav__underline}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ ease: 'linear', duration: 0.5, delay: 2 }}
+          />}
+        </motion.li>)}
+    </ul>
   </nav>
 }
