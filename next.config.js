49134f49a78c51bf470b559ba06f4846a067322d
Mauro Colella <mauro.ludovico.colella@gmail.com>
2023-01-03 13:46:37 +0800

    Switch runtime

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = {
   experimental: {
     appDir: true,
-    runtime: 'edge',
+    runtime: 'edge-experimental',
   },
   reactStrictMode: false,
 }
